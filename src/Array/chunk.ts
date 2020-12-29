27c704064304a4dab35e729b67fc1ab31e55fb46
郑云 <zhengyunk@yonyou.com>
2020-12-29 17:09:17 +0800

    refactor: 新增部分工具函数

---
+++ src/Array/chunk.ts
@@ -0,0 +1,12 @@
+/**
+ * @description: 
+ * @param {*} array
+ * @param {*} size
+ * @return {*}
+ * @example: 
+ */
+function chunk(array: Array<any>, size: number = 1): Array<any> {
+    return [1, 2, 3]
+}
+
+export default chunk;
---
+++ src/Array/slice.ts
@@ -0,0 +1,10 @@
+/*
+ * @Description:
+ * @Version: 2.0
+ * @Autor: ZhengYun
+ * @Date: 2020-12-29 16:57:03
+ * @LastEditTime: 2020-12-29 16:59:54
+ */
+export default function slice(array: Array<any>, start: number, end: number) {
+
+}
